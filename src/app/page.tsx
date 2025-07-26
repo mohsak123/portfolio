68a165947218615718e1ef4333c926a03eaf9d63
wajihhawarii <wajihhoware72@gmail.com>
2025-07-26 21:51:29 +0300

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
+// import React from 'react'
 
-import Hero from '@/components/Hero';
-import Main from '@/components/Main';
-import ContactUs from '@/components/ContactUs';
-import ScrollButton from '@/components/ScrollButton';
+// import Hero from '@/components/Hero';
+// import Main from '@/components/Main';
+// import ContactUs from '@/components/ContactUs';
+// import ScrollButton from '@/components/ScrollButton';
 
-const page = () => {
-  return (
-    <div className=''>
-      <Hero />
-      <div className='divider' />
-      <Main />
-      <div className='divider' />
-      <ContactUs />
-      <div className='divider' />
-      <ScrollButton />
-    </div>
-  )
-}
+// const page = () => {
+//   return (
+//     <div className=''>
+//       <Hero />
+//       <div className='divider' />
+//       <Main />
+//       <div className='divider' />
+//       <ContactUs />
+//       <div className='divider' />
+//       <ScrollButton />
+//     </div>
+//   )
+// }
 
-export default page
+// export default page
